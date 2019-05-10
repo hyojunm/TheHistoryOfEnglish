342f3122826aaf671ff062171aae10eddcebdb25
Jeremy Moon <34760259+hyojunm@users.noreply.github.com>
2019-05-10 16:26:15 -0500

    A few minor updates

--- files/code/breakout.js
+++ files/code/breakout.js
@@ -11,5 +11,16 @@ $(document).ready(function() {
 	$("#toLockI").click(function() {
 		$("#story").hide();
 		$("#lock1").fadeIn(2000);
-	})
+	});
+
+	$("#unlock1").click(function() {
+		var lockCombo1 = $("#LockI").val();
+		if (lockCombo1 == "1066") {
+			$(".incorrectMsg").text("");
+			$("lock1").hide();
+			$("lock2").fadeIn(2000);
+		} else {
+			$(".incorrectMsg").text("The lock combination is incorrect. Please try again.")
+		}
+	});
 });
